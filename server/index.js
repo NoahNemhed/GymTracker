10209ad533af4b1cfea17b0804942923d793f83f
NoahNemhed <noahnemhed@hotmail.com>
2026-04-07 17:34:23 +0200

    feat: connect Express server to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
 
 const app = express();
 
