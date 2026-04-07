24dfdfb3a1ef43454b419927310f7a77843d872b
NoahNemhed <noahnemhed@hotmail.com>
2026-04-07 21:04:28 +0200

    feat: add filtering on muscle and exerciseType to exercises endpoint

--- server/controllers/exerciseController.js
+++ server/controllers/exerciseController.js
@@ -2,9 +2,40 @@ import Exercise from "../models/Exercise.js";
 
 export const getExercises = async (req, res) => {
   try {
-    const exercises = await Exercise.find();
+    const { muscle, type } = req.query;
+
+    let filter = {};
+
+    // Filter by primary muscle
+    if (muscle) {
+      filter.primaryMuscles = muscle;
+    }
+
+    // filter by exercise type e.g compound / isolation / conditioning.
+    if (type) {
+      filter.exerciseType = type;
+    }
+
+    const exercises = await Exercise.find(filter);
+
     res.status(200).json(exercises);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch exercises" });
+  }
+};
+
+export const getExerciseById = async (req, res) => {
+  try {
+    const exercise = await Exercise.findOne({
+      exerciseId: req.params.id,
+    });
+
+    if (!exercise) {
+      return res.status(404).json({ message: "Exercise not found" });
+    }
+
+    res.status(200).json(exercise);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch exercise" });
   }
 };
--- server/routes/exerciseRoutes.js
+++ server/routes/exerciseRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getExercises } from "../controllers/exerciseController.js";
+import { getExercises, getExerciseById } from "../controllers/exerciseController.js";
 
 const router = express.Router();
 
 router.get("/", getExercises);
+
+router.get("/:id", getExerciseById);
 
 export default router;
